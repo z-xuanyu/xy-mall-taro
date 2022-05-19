5ebf670f52e8ac376a405f2b901dfa04b18466e0
xuanyu <969718197@qq.com>
2022-05-19 11:58:56 +0800

    feature: 添加订单列表页面

--- src/pages/order/list/index.config.ts
+++ src/pages/order/list/index.config.ts
@@ -4,7 +4,7 @@
  * @email: 969718197@qq.com
  * @github: https://github.com/z-xuanyu
  * @Date: 2022-05-17 10:54:01
- * @LastEditTime: 2022-05-17 10:54:01
+ * @LastEditTime: 2022-05-19 11:55:09
  * @Description: Modify here please
  */
 export default definePageConfig({
